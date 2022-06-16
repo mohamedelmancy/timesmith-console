4d734a9d23186ec335ddaad1be51854d65a9e063
mohamedelmancy <mohamedelmancy998@gmail.com>
2022-06-16 04:16:45 +0200

    work in site map and current location

--- src/app/modules/configurations/sites/create-site/create-site.component.ts
+++ src/app/modules/configurations/sites/create-site/create-site.component.ts
@@ -6,8 +6,7 @@ import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import {ActivatedRoute, Router} from "@angular/router";
 import {CoreService} from "../../../../services/core.service";
 import {secureStorage} from "../../../../shared/functions/secure-storage";
-import {LatLng} from "leaflet";
-
+import 'leaflet.locatecontrol'
 @Component({
   selector: 'app-create-site',
   templateUrl: './create-site.component.html',
@@ -22,10 +21,12 @@ export class CreateSiteComponent implements OnInit, AfterViewInit {
   map;
   data;
   marker;
+  circle;
+  defaultIcon = new L.Icon.Default();
   customIcon = L.icon({
     iconUrl: '../../../../../assets/img/map/marker.svg',
-    iconSize: [30, 30],
-    iconAnchor: [10, 10],
+    iconSize: [40, 40],
+    iconAnchor: [20, 20],
     popupAnchor: [0, -60],
     shadowUrl: '../../../../../assets/img/map/marker-shadow.png'
   });
@@ -42,28 +43,51 @@ export class CreateSiteComponent implements OnInit, AfterViewInit {
     setTimeout(() => {
       this.map = L.map('map', {
         center: [this.lat, this.lng],
-        zoom: 12
+        zoom: 12,
       });
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {
         foo: 'bar',
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(this.map);
-      // L.control.locate({ enableHighAccuracy: true }).addTo(this.map);
       const _this = this;
       this.map.on('click', function (e) {
         console.log('eee', e);
-        _this.form.controls['latitude'].setValue(e.latlng?.lat);
-        _this.form.controls['longitude'].setValue(e.latlng?.lng);
-        _this.addMarker(e);
+       _this.fillMapFields(e);
       });
+      L.control.locate().addTo(this.map);
       this.addSearchBox();
       this.initialize();
-      this.gotoCurrent();
+      this.map?.on('locationfound', function(ev){
+        console.log('locationfound', ev);
+        _this.onLocationFound(ev);
+      })
+      // this.gotoCurrent();
     }, 2000);
   }
 
-  ngAfterViewInit() {
+  onLocationFound(e) {
+    var radius = e.accuracy / 2;
+    L.marker(e.latlng, {icon: this.customIcon}).addTo(this.map)
+      .bindPopup("You are within " + radius + " meters from this point").openPopup();
+    this.addCircle(e);
+  }
 
+  addCircle(e) {
+    var radius = (e.accuracy / 2) || (this.form.value.tolerance) || 30;
+    if (!this.circle) {
+      this.circle = L.circle(e.latlng, radius).addTo(this.map);
+    } else {
+      this.circle.setLatLng(e.latlng)
+    }
+  }
+
+  ngAfterViewInit() {
+  }
+
+  fillMapFields(e) {
+    this.form.controls['latitude'].setValue(e.latlng?.lat);
+    this.form.controls['longitude'].setValue(e.latlng?.lng);
+    this.addMarker(e);
   }
 
   initialize() {
@@ -72,42 +96,36 @@ export class CreateSiteComponent implements OnInit, AfterViewInit {
     console.log('this.data', this.data);
     this.form.valueChanges.subscribe(changes => {
       if (changes.latitude && changes?.longitude) {
-        // console.log('changes', changes);
+        console.log('changes', changes);
         this.map?.panTo(new L.LatLng(changes?.latitude, changes?.longitude));
         this.addMarker({latlng: {lat: changes.latitude, lng: changes.longitude}});
+        // this.addCircle({latlng: {lat: changes.latitude, lng: changes.longitude}}, this.form.value.tolerance || 15);
       }
     });
     this.fillForm();
   }
 
   addMarker(ev) {
-    const _this = this;
-    var icon = new L.Icon.Default();
+    var radius = this.form.value.tolerance || 30;
     if (!this.marker) {
-      console.log('111', ev);
-      this.marker = L.marker([ev.latlng.lat, ev.latlng.lng], {icon: this.customIcon, draggable: true, id: Math.random()}).addTo(_this.map);
+      this.marker = L.marker([ev.latlng.lat, ev.latlng.lng], {icon: this.customIcon, draggable: true, id: Math.random()}).addTo(this.map)
+        .bindPopup("You are within " + radius + " meters from this point").closePopup();
     } else {
-      // this.map.removeLayer(this.marker)
-      console.log('2222', ev);
       this.marker.setLatLng(ev.latlng)
     }
-
+    this.addCircle(ev);
   }
 
   gotoCurrent() {
-    const _this = this;
-    this.map.on('locationfound', function(ev){
-        console.log('locationfound', ev);
+    // const locate = this.map.locate({setView: true, maxZoom: 12});
+    // console.log('locate', locate)
 
-    })
+    const _this = this;
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log('position', position)
-      const latit = position.coords.latitude;
-      const longit = position.coords.longitude;
-      // this is just a marker placed in that position
-      var abc = L.marker([position.coords.latitude, position.coords.longitude]).addTo(_this.map);
-      // move the map to have the location in its center
-      _this.map.panTo(new L.LatLng(latit, longit));
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+      _this.fillMapFields({latlng: {lat, lng}})
     })
   }
 
@@ -134,14 +152,19 @@ export class CreateSiteComponent implements OnInit, AfterViewInit {
         icon: this.customIcon,
         draggable: true,
       },
-      popupFormat: ({ query, result }) => result.label, // optional: function    - default returns result label,
-      resultFormat: ({ result }) => result.label, // optional: function    - default returns result label
+      popupFormat: ({ query, result }) => {
+        return result.label
+      }, // optional: function    - default returns result label,
+      resultFormat: ({ result }) => {
+        // console.log('result.label', result.label);
+        return result.label;
+      }, // optional: function    - default returns result label
       maxMarkers: 1, // optional: number      - default 1
       retainZoomLevel: true, // optional: true|false  - default false
       animateZoom: true, // optional: true|false  - default true
       autoClose: false, // optional: true|false  - default false
       searchLabel: 'Enter address', // optional: string      - default 'Enter address'
-      keepResult: false, // optional: true|false  - default false
+      keepResult: true, // optional: true|false  - default false
       updateMap: true, // optional: true|false  - default true
     }).addTo(this.map);
   }
